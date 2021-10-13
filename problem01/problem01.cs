297cfd6646a9fc5d20450655efc25d6f0fc1ca41
Voranipit Ch <62955654+blxsheep@users.noreply.github.com>
2021-10-13 19:56:31 +0700

    16 Threads
    Use 16 Threads for best performance.

--- problem01/problem01.cs
+++ problem01/problem01.cs
@@ -10,9 +10,13 @@ namespace Problem01
     class Program
     {
         static byte[] Data_Global = new byte[1000000000];
-        static long Sum_Global = 0;
-        //static int G_index = 0;
+        static long[] Sum_Global = new long[1000];
+        static int G_index = 0;
 
+        
+
+        static int cores = 16;
+        static int divider = 1000000000 / cores;
         static int ReadData()
         {
             int returnData = 0;
@@ -35,62 +39,44 @@ namespace Problem01
 
             return returnData;
         }
-        static void sum(int start, int end)
+        static void sum(ref long psum, int idx)
         {
-           // Console.Write("I'm in Loop now {0}",start);
-            for (int G_index = start; G_index <= end; G_index += 1)
+            // time reduced by changed data ref method
+            int val = Data_Global[idx];
+            if (val % 2 == 0)
             {
-                if (Data_Global[G_index] % 2 == 0)
-                {
-                    Sum_Global -= Data_Global[G_index];
-                }
-                else if (Data_Global[G_index] % 3 == 0)
-                {
-                    Sum_Global += (Data_Global[G_index] * 2);
-                }
-                else if (Data_Global[G_index] % 5 == 0)
-                {
-                    Sum_Global += (Data_Global[G_index] / 2);
-                }
-                else if (Data_Global[G_index] % 7 == 0)
-                {
-                    Sum_Global += (Data_Global[G_index] / 3);
-                }
-                Data_Global[G_index] = 0;
-                G_index++;
+                psum -= val;
+            }
+            else if (val % 3 == 0)
+            {
+                psum += (val * 2);
+            }
+            else if (val % 5 == 0)
+            {
+                psum += (val / 2);
+            }
+            else if (val % 7 == 0)
+            {
+                psum += (val / 3);
+            }
+            Data_Global[idx] = 0;
+        }
+
+        static void task(ref long psum, int start, int stop)
+        {
+            for (int i = start; i < stop; ++i)
+            {
+                sum(ref psum, i);
             }
         }
-        // static void sum()
-        // {
-            
-        //         if (Data_Global[G_index] % 2 == 0)
-        //         {
-        //             Sum_Global -= Data_Global[G_index];
-        //         }
-        //         else if (Data_Global[G_index] % 3 == 0)
-        //         {
-        //             Sum_Global += (Data_Global[G_index] * 2);
-        //         }
-        //         else if (Data_Global[G_index] % 5 == 0)
-        //         {
-        //             Sum_Global += (Data_Global[G_index] / 2);
-        //         }
-        //         else if (Data_Global[G_index] % 7 == 0)
-        //         {
-        //             Sum_Global += (Data_Global[G_index] / 3);
-        //         }
-        //         Data_Global[G_index] = 0;
-        //         G_index++;
-            
-        // }
+
+
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
-            int i, y;
-            const int N = 1000000000;
-            int cores = 10 ; 
-            int range =  N/cores;
-            const int  approxDefaultRuntime =43000 ; 
+            int y;
+            const long correct = 888701676;
+            const int approxDefaultRuntime = 43000;
 
             /* Read data from file */
             Console.Write("Data read...");
@@ -104,31 +90,58 @@ namespace Problem01
                 Console.WriteLine("Read Failed!");
             }
 
+
+
+            Thread[] threads = new Thread[10000];
+
+            for (int k = 0; k < cores; k += 1)
+            {
+                int yo = k;
+                Console.WriteLine("Create");
+                Thread t = new Thread(() =>
+                {
+                    task(ref Sum_Global[yo], divider * (yo), divider * (yo + 1));
+                });
+                threads[yo] = t;
+            }
+
+
             /* Start */
-            Thread[] threads = new Thread[cores];
             Console.Write("\n\nWorking...");
             sw.Start();
-            //for (i=0; i < N;i++)
-            //    sum();
-            for ( i = 0; i < cores; i++){
-                //Console.WriteLine(i);
-               // Console.WriteLine("new Thread Start");
-               // Console.WriteLine(i);
-                Thread t = new Thread( ()=> sum(i*range,i*range+range-1)) ;
-                t.Start();
-                threads[i] = t;
-               // Console.WriteLine("Out of function now ");
 
+            for (int j = 0; j < cores; j += 1)
+            {
+                Console.WriteLine("Start");
+                try
+                {
+                    threads[j].Start();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error");
+                }
             }
-                
+            for (int h = 0; h < cores; h += 1)
+            {
+                Console.WriteLine("Join");
+                threads[h].Join();
+            }
+
+
             sw.Stop();
             Console.WriteLine("Done.");
 
             /* Result */
-            
-            Console.WriteLine("Summation result: {0}", Sum_Global);
+            long s = 0;
+            Array.ForEach(Sum_Global, i => s += i);
+            Console.WriteLine("Threads count : {0}", cores);
+            Console.WriteLine("Summation result: {0}", s);
             Console.WriteLine("Time used: " + sw.ElapsedMilliseconds.ToString() + "ms");
-            Console.WriteLine("Approximate Defualt Time before Thread: {0} ms",approxDefaultRuntime);
+            Console.WriteLine("Time Reduced: {0}%", ((1 - (sw.ElapsedMilliseconds * 1.0) / (approxDefaultRuntime * 1.0)) * 100).ToString("N2"));
+            Console.WriteLine("Summation Correctness: {0}", s == correct);
         }
+
+
     }
 }
